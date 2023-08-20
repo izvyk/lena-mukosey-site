1b4bb58737f8e44424a84088771cf7594f2e0bf9
Philipp Mukosey <philipp@mukosey.com>
2023-08-20 16:29:39 +0300

    Image alt-related changes

--- .eleventy.js
+++ .eleventy.js
@@ -171,7 +171,6 @@ module.exports = function (eleventyConfig) {
         <img
           class="placeholder"
           src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${highsrc.width}' height='${highsrc.height}'%3E%3Crect width='100%25' height='100%25' fill='%230000'/%3E%3C/svg%3E"
-          alt="${alt}"
         >
       </div>`;
 	});
