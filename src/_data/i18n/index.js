767ccd3eaac284d5abf787756e5681413df9bc35
Philipp Mukosey <philipp@mukosey.com>
2024-07-14 13:50:05 +0200

    i18n dictionary & <meta> translations

---
+++ src/_data/i18n/index.js
@@ -0,0 +1,34 @@
+module.exports = {
+    series_length: {
+        'en': 'A series of {{ number }} artworks',
+        'ru': 'Серия из {{ number }} работ',
+    },
+    size: {
+        mm: {
+            'en': 'mm',
+            'ru': 'мм',
+        },
+        cm: {
+            'en': 'cm',
+            'ru': 'см',
+        },
+        m: {
+            'en': 'm',
+            'ru': 'м',
+        },
+    },
+    meta: {
+        author: {
+            'en': 'Lena Mukosey',
+            'ru': 'Лена Мукосей',
+        },
+        description: {
+            'en': 'Lena Mukosey (ducklusha): graphics, painting, illustration, body art',
+            'ru': 'Лена Мукосей: графика, живопись, иллюстрация, боди-арт',
+        },
+        keywords: {
+            'en': 'lena mukosey, art, artist, artwork, contemporary art, contemporary artist, abstract art, graphics, painting, illustration, body art, ducklusha',
+            'ru': 'лена мукосей, графика, живопись, иллюстрация, боди-арт, современное искусство, искусство, современный художник, современная художница, художница',
+        },
+    },
+  };
