e6117db682476e9164f90043280f9440a485d813
Philipp Mukosey <philipp@mukosey.com>
2024-07-17 00:12:29 +0200

    Image load animation fix

--- src/assets-inline/script/index.js
+++ src/assets-inline/script/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', _ => 
     document.querySelectorAll('.card .imageContainer img').forEach(
-        img => img.addEventListener('load', _ => img.style.opacity = 1)
+        img => img.complete ?
+            img.style.opacity = 1 : img.addEventListener('load', _ => img.style.opacity = 1, {once: true})
     )
 );
